bbc979e984964ce75335756bb253d06f0196b127
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:40 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "stack_hci_deployment_settings" {
-  description = "All stack_hci_deployment_setting resources"
-  value       = azurerm_stack_hci_deployment_setting.stack_hci_deployment_settings
-}
 output "stack_hci_deployment_settings_arc_resource_ids" {
-  description = "List of arc_resource_ids values across all stack_hci_deployment_settings"
-  value       = [for k, v in azurerm_stack_hci_deployment_setting.stack_hci_deployment_settings : v.arc_resource_ids]
+  description = "Map of arc_resource_ids values across all stack_hci_deployment_settings, keyed the same as var.stack_hci_deployment_settings"
+  value       = { for k, v in azurerm_stack_hci_deployment_setting.stack_hci_deployment_settings : k => v.arc_resource_ids }
 }
 output "stack_hci_deployment_settings_scale_unit" {
-  description = "List of scale_unit values across all stack_hci_deployment_settings"
-  value       = [for k, v in azurerm_stack_hci_deployment_setting.stack_hci_deployment_settings : v.scale_unit]
+  description = "Map of scale_unit values across all stack_hci_deployment_settings, keyed the same as var.stack_hci_deployment_settings"
+  value       = { for k, v in azurerm_stack_hci_deployment_setting.stack_hci_deployment_settings : k => v.scale_unit }
 }
 output "stack_hci_deployment_settings_stack_hci_cluster_id" {
-  description = "List of stack_hci_cluster_id values across all stack_hci_deployment_settings"
-  value       = [for k, v in azurerm_stack_hci_deployment_setting.stack_hci_deployment_settings : v.stack_hci_cluster_id]
+  description = "Map of stack_hci_cluster_id values across all stack_hci_deployment_settings, keyed the same as var.stack_hci_deployment_settings"
+  value       = { for k, v in azurerm_stack_hci_deployment_setting.stack_hci_deployment_settings : k => v.stack_hci_cluster_id }
 }
 output "stack_hci_deployment_settings_version" {
-  description = "List of version values across all stack_hci_deployment_settings"
-  value       = [for k, v in azurerm_stack_hci_deployment_setting.stack_hci_deployment_settings : v.version]
+  description = "Map of version values across all stack_hci_deployment_settings, keyed the same as var.stack_hci_deployment_settings"
+  value       = { for k, v in azurerm_stack_hci_deployment_setting.stack_hci_deployment_settings : k => v.version }
 }
 
